7ea5ad94ef91015b90981b79dca48e283c717ce8
Daniel Bedggood <ddbedggood@gmail.com>
2024-07-29 18:33:11 +1200

    Add CollisionPolygon2D for body

--- project/snake/body.gd
+++ project/snake/body.gd
@@ -6,10 +6,35 @@ signal grown
 @onready var head = $"../Head"
 @onready var tail = $"../Tail"
 
+@onready var body_collisions = $BodyArea/BodyCollisions
+
 
 func _process(_delta):
 	set_point_position(0, head.get_point_position(Constants.HEAD_BASE_POINT_INDEX))
-	snake.set_constrained_nodes(self, Constants.BODY_MAX_POINT_DISTANCE)
+	snake.set_constrained_nodes_with_collisions(self, Constants.BODY_MAX_POINT_DISTANCE)
+
+	var snake_body_point_directions = snake.body_point_directions
+
+	if !snake_body_point_directions.is_empty():
+		var body_point_count = get_point_count()
+		var new_body_collisions_polygon: PackedVector2Array = body_collisions.polygon
+
+		for point_index in range(1, body_point_count * 2 - 1):
+			if point_index < body_point_count:
+				var point_position = get_point_position(point_index)
+				var collision_position = (
+					point_position + snake_body_point_directions[point_index - 1].orthogonal() * 8
+				)
+				new_body_collisions_polygon.set(point_index - 1, collision_position)
+			else:
+				var reverse_point_index = body_point_count * 2 - (point_index + 1)
+				var point_position = get_point_position(reverse_point_index)
+				var collision_position = (
+					point_position + snake_body_point_directions[point_index - 1].orthogonal() * -8
+				)
+				new_body_collisions_polygon.set(point_index - 1, collision_position)
+
+		body_collisions.polygon = new_body_collisions_polygon
 
 
 func _on_snake_ate_cherries():
@@ -21,4 +46,13 @@ func _on_snake_ate_cherries():
 func grow_body():
 	var new_point_position = tail.get_point_position(1)
 	add_point(new_point_position)
+
+	var new_body_collisions_polygon: PackedVector2Array = body_collisions.polygon
+
+	# TODO: insert these at the right location
+	new_body_collisions_polygon.append(Vector2.ZERO)
+	new_body_collisions_polygon.append(Vector2.ZERO)
+
+	body_collisions.set_deferred("polygon", new_body_collisions_polygon)
+
 	grown.emit()
--- project/snake/snake.gd
+++ project/snake/snake.gd
@@ -2,14 +2,44 @@ extends Node2D
 
 signal ate_cherries
 
+var body_point_directions = []
+
+
+# TODO: refactor
+func set_constrained_nodes_with_collisions(target_node, max_point_distance):
+	var current_body_point_directions = []
+	var reverse_body_point_directions = []
+
+	for point_index in range(1, target_node.get_point_count()):
+		var previous_point_position = target_node.get_point_position(point_index - 1)
+		var current_point_position = target_node.get_point_position(point_index)
+		var previous_to_current_direction = (
+			(current_point_position - previous_point_position).normalized()
+		)
+
+		# saving to calculate collision shape
+		current_body_point_directions.append(previous_to_current_direction)
+		reverse_body_point_directions.push_front(previous_to_current_direction)
+
+		var constrained_new_position = (
+			previous_point_position + previous_to_current_direction * max_point_distance
+		)
+		target_node.set_point_position(point_index, constrained_new_position)
+
+	current_body_point_directions.append_array(reverse_body_point_directions)
+	if body_point_directions != current_body_point_directions:
+		body_point_directions = current_body_point_directions
+
 
 func set_constrained_nodes(target_node, max_point_distance):
 	for point_index in range(1, target_node.get_point_count()):
 		var previous_point_position = target_node.get_point_position(point_index - 1)
 		var current_point_position = target_node.get_point_position(point_index)
-		var previous_to_current_vector = current_point_position - previous_point_position
+		var previous_to_current_direction = (
+			(current_point_position - previous_point_position).normalized()
+		)
 		var constrained_new_position = (
-			previous_point_position + previous_to_current_vector.normalized() * max_point_distance
+			previous_point_position + previous_to_current_direction * max_point_distance
 		)
 		target_node.set_point_position(point_index, constrained_new_position)
 
